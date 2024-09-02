2d35fb0ed2c9d978631582ae4114e5cbd94d8a40
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-02 16:23:42 +0300

    done alerts

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
-export function createHome() {
+export function createHome(alert) {
 
     let container = document.querySelector(".container");
 
-
     container.innerHTML = `
     
+       <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+       </div>  
+
     	<h1>Doctors</h1>
 
     <button class="button"> Add doctor</button>
@@ -24,22 +27,80 @@ export function createHome() {
     
     `
 
+    let button = document.querySelector(".button");
+    let table = document.querySelector(".table");
+    const alertPlaceholder = document.querySelector('.container-alert');
+    let load = document.querySelector(".spinner-border");
+
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
+
     api("https://localhost:7111/api/v1/Doctor/all").then(response => {
         return response.json();
     }).then(data => {
-        console.log(data);
-
+        load.classList = "";
+        //console.log(data);
         attachDoctors(data.doctorList);
+
     }).catch(error => {
+
+        load.classList = "";
+
         console.error('Error fetching data:', error);
+
+        appendAlert(error, "danger");
     });
-
-
-    let button = document.querySelector(".button");
 
     button.addEventListener("click", (eve) => {
         CreateAddDoctorPage();
     });
+
+    table.addEventListener("click", (eve) => {
+
+        if (eve.target.classList.contains("updateDoc")) {
+            api(`https://localhost:7111/api/v1/Doctor/id/${eve.target.textContent}`).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+
+                let doctor = {
+                    name: data.name,
+                    patients: data.patients,
+                    type: data.type
+                }
+
+                CreateUpdatePage(doctor, eve.target.textContent);
+
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        }
+
+    });
+
+    if (alert === "deleted") {
+        load.classList = "";
+        appendAlert("Book has been DELETED with success!", "success");
+    }
+
+    if (alert === "updated") {
+        load.classList = "";
+        appendAlert("Book has been UPDATED with success!", "success");
+    }
+
+    if (alert === "added") {
+        load.classList = "";
+        appendAlert("Book has been ADDED with success!", "success");
+    }
 
 }
 
@@ -81,12 +142,86 @@ export function CreateAddDoctorPage() {
     let test = document.querySelector(".createDoctor");
 
     button.addEventListener("click", (eve) => {
-        createHome();
+        createHome("");
     })
 
     test.addEventListener("click", (eve) => {
-        createDoctor();
+        createUpdateDoctor("create");
     })
+
+}
+
+export function CreateUpdatePage(doctor, idDoctor) {
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = `
+    <h1>Update Doctor</h1>
+    <form>
+        <p>
+            <label for="name">Name</label>
+            <input name="name" type="text" id="name" value="${doctor.name}">
+             <a class="nameErr">Name required!</a>
+        </p>
+        <p>
+            <label for="typeDoc">Type</label>
+            <input name="typeDoc" type="text" id="typeDoc" value="${doctor.type}">
+             <a class="typeErr">Type required!</a>
+        </p>
+        <p>
+            <label for="patients">Patients</label>
+            <input name="patients" type="text" id="patients" value="${doctor.patients}">
+             <a class="patientsErr">Patients required!</a>
+        </p>
+
+        <div class="submitUpdate">
+         <a href="#">Update Doctor</a>
+        </div>
+
+          <div class="cancel">
+         <a href="#">Cancel</a>
+        </div>
+        <div class="submitDelete">
+         <a href="#">Delete Doctor</a>
+        </div>
+    </form>
+    `
+
+    let cancelButton = document.querySelector(".cancel");
+    let submitUpdateButton = document.querySelector(".submitUpdate");
+    let submitDeleteButton = document.querySelector(".submitDelete");
+    let nameinput = document.getElementById("name");
+    let typeinput = document.getElementById("typeDoc");
+
+    nameinput.disabled = true;
+    typeinput.disabled = true;
+
+    cancelButton.addEventListener("click", (eve) => {
+        createHome("");
+    });
+
+    submitUpdateButton.addEventListener("click", (eve) => {
+        createUpdateDoctor("update", idDoctor);
+    });
+
+    submitDeleteButton.addEventListener("click", (eve) => {
+
+        api(`https://localhost:7111/api/v1/Doctor/delete/${idDoctor}`, "DELETE")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+
+                createHome("deleted");
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    })
+
 
 }
 
@@ -97,7 +232,7 @@ function createRow(doctor) {
 
     tr.innerHTML = `
   
-				<td>${doctor.id}</td>
+				<td class="updateDoc">${doctor.id}</td>
 				<td>${doctor.name}</td>
 				<td>${doctor.type}</td>
 				<td>${doctor.patients}</td>
@@ -139,7 +274,7 @@ function attachDoctors(doctors) {
 
 }
 
-function createDoctor() {
+function createUpdateDoctor(request, idDoctor) {
 
     const isNumber = (str) => {
         return /^[+-]?\d+(\.\d+)?$/.test(str);
@@ -210,17 +345,31 @@ function createDoctor() {
             type: type
         }
 
-        api("https://localhost:7111/api/v1/Doctor/create", "POST", doctor)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                createHome();
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        if (request === "create") {
+            api("https://localhost:7111/api/v1/Doctor/create", "POST", doctor)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("added");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else if (request === "update") {
+            api(`https://localhost:7111/api/v1/Doctor/update/${idDoctor}`, "PUT", doctor)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("updated");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     } else {
 
         errors.forEach(err => {
@@ -251,5 +400,4 @@ function createDoctor() {
     }
 
 }
-
 
